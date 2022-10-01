a8d61625c5a87d5408d54e9139fbdb1d1c3cb2c6
chaneeii <chan420420@gamil.com>
2022-10-01 20:35:38 +0900

    Initial Commit

---
+++ Summer-iOS/Summer_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Summer_iOSApp.swift
+//  Summer-iOS
+//
+//  Created by Chanhee Jeong on 2022/10/01.
+//
+
+import SwiftUI
+
+@main
+struct Summer_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
